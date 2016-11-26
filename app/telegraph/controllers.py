aa2901a7c7d622565fbecf3049db749d87d79c53
Francisco <andrewplanet@gmail.com>
2016-11-26 15:11:55 -0400

    Add methods

--- app/telegraph/controllers.py
+++ app/telegraph/controllers.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from app import db
 from app.telegraph.model import Post
 
 telegraph = Blueprint("telegraph", __name__)
 
-@telegraph.route("/")
+@telegraph.route("/", methods = ["POST", "GET"])
 def index():
 	return render_template("telegraph/index.html")
