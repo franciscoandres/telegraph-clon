75b8627e9987a539f849bb5e2a3a8ff5d6dfaada
Francisco <andrewplanet@gmail.com>
2016-11-26 15:10:57 -0400

    Moar config stuff

--- config.py
+++ config.py
@@ -2,5 +2,10 @@ import os
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
+CSRF_ENABLED = True
+CSRF_SESSION_KEY = "secretcsrfhere"
+
+SECRET_KEY = "secretkeyhere"
+
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "app.sqlite")
 SQLALCHEMY_TRACK_MODIFICATIONS = True
