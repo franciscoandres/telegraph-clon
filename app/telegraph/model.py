1524c7d8db1e9a272eb4fe60ee4caab1a79863b4
Francisco <andrewplanet@gmail.com>
2016-11-26 16:19:02 -0400

    Add slug

--- app/telegraph/model.py
+++ app/telegraph/model.py
@@ -5,14 +5,15 @@ class Post(db.Model):
 	__tablename__ = "post"
 
 	id        = db.Column(db.Integer, primary_key = True)
-	slug      = db.Column(db.String(256), nullable = False)
+	slug      = db.Column(db.String(256), nullable = False, unique = True)
 	title     = db.Column(db.String(256), nullable = False)
 	author    = db.Column(db.String(128), nullable = False)
 	create_at = db.Column(db.DateTime(), default = db.func.current_timestamp())
 	content   = db.Column(db.Text())
 
-	def __init__(self, title, author, content):
+	def __init__(self, title, slug, author, content):
 		self.title   = title
+		self.slug    = slug
 		self.author  = author
 		self.content = content
 
