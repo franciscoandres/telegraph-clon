7844f57b1ff11f33d6338dda064051dbb1f68bb7
Francisco <andrewplanet@gmail.com>
2016-11-27 17:05:28 -0400

    Added 404 error page

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
-from flask import Flask 
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object("config")
 
 db = SQLAlchemy(app)
+
+@app.errorhandler(404)
+def not_found(error):
+	return render_template("404.html"), 404
 
 from app.telegraph.controllers import telegraph
 
