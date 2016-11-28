a995c474d5576f335a67a0302962f2f658c53667
Francisco <andrewplanet@gmail.com>
2016-11-28 14:10:52 -0400

    Add moar validations

--- app/telegraph/forms.py
+++ app/telegraph/forms.py
@@ -1,8 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 class PostForm(FlaskForm):
-    title   = StringField("title", validators=[DataRequired(message="Title field is required")])
-    author  = StringField("author", validators=[DataRequired(message="Author field is required")])
+    title   = StringField("title", validators=[
+        DataRequired(message="Title field is required"),
+        Length(min = 3, max = 256, message = "Minimum 3 characters")
+        ])
+    author  = StringField("author", validators=[
+        DataRequired(message="Author field is required"),
+        Length(min = 3, max = 256, message = "Minimum 3 characters")
+        ])
     content = TextAreaField("content", validators=[DataRequired(message="Content field is required")])
