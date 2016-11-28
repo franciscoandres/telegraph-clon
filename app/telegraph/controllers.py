ff0fd71476d6562d1316aa26f9ebbbe420fb0318
Francisco <andrewplanet@gmail.com>
2016-11-28 09:25:08 -0400

    Change some route names and files, now makes more sense

--- app/telegraph/controllers.py
+++ app/telegraph/controllers.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, session
+from flask import Blueprint, render_template, request, redirect, url_for, session, flash
 
 import app.utils as utils
 
@@ -8,29 +8,26 @@ from app.telegraph.model import Post
 telegraph = Blueprint("telegraph", __name__)
 
 @telegraph.route("/", methods = ["POST", "GET"])
-def index():
+def new_post():
 
 	if request.method == "POST":
-		title   = request.form["title"]
-		slug    = title.lower().strip().replace(" ", "-")
-		author  = request.form["author"]
-		content = request.form["content"]
+		title      = request.form["title"]
+		slug       = title.lower().strip().replace(" ", "-")
+		author     = request.form["author"]
+		content    = request.form["content"]
 
 		post = Post(title, slug, author, content)
 		db.session.add(post)
 		db.session.commit()
 
-		session["edit"] = utils.generate_string()
-
 		return redirect(url_for("telegraph.get_post", slug = slug))
 
-	return render_template("telegraph/index.html")
+	return render_template("telegraph/new.html")
 
 @telegraph.route("/<slug>/")
 def get_post(slug):
-	
 	post = Post.query.filter_by(slug = slug).first_or_404()
-	return render_template("telegraph/post.html", post = post)
+	return render_template("telegraph/single.html", post = post)
 
 @telegraph.route("/<slug>/edit/", methods = ["POST", "GET"])
 def edit_post(slug):
